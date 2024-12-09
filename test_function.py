2c1c987d52f3db1bfd7338d1c734cf94d618d3c4
Sanhanat-Somnee-66026189 <66026189@up.ac.th>
2024-12-09 21:30:40 +0700

    Test calAge,calRank

--- test_function.py
+++ test_function.py
@@ -1,4 +1,24 @@
 from quiztest import *
-file = open("Textfile.txt","r")
+
 def test01():
-    assert ShowEditedData(file) == ''
+    assert calAge(20) == '2547'
+def test02():
+    assert calAge("20") == '2547'
+
+
+def test03():
+    assert calRank("A") == 'High Distinction'
+def test04():
+    assert calRank("B+") == 'Distinction'
+def test05():
+    assert calRank("B") == 'Distinction'
+def test06():
+    assert calRank("C+") == 'Good'
+def test07():
+    assert calRank("C") == 'Good'
+def test07():
+    assert calRank("D+") == 'Normal'
+def test07():
+    assert calRank("D") == 'Normal'
+def test07():
+    assert calRank("F") == 'Failed'
